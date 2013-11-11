f2ac50016e9da8728091f1eca6b5416d1e216ce4
Andrea Gottardi <andreagot16@gmail.com>
2013-11-11 16:39:00 +0100

    MySQL on JDBC works properly, both on local and web solutions.

---
+++ MySQLandJDBCWeb/src/java/ServletDB.java
@@ -0,0 +1,221 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ANDre1
+ */
+public class ServletDB extends HttpServlet {
+
+    
+    
+    
+    
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    public void readDataBase() throws Exception {
+        String url = "jdbc:mysql://localhost:8889/feedback";
+        String userpass = "root";
+        try {
+            System.out.println("Loading driver...");
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot find the driver in the classpath!", e);
+        }
+        try {
+            // This will load the MySQL driver, each DB has its own driver
+            // Setup the connection with the DB
+            connect = DriverManager.getConnection(url, userpass, userpass);
+
+            // Statements allow to issue SQL queries to the database
+            statement = connect.createStatement();
+            // Result set get the result of the SQL query
+            resultSet = statement
+                    .executeQuery("select * from feedback.COMMENTS");
+            writeResultSet(resultSet);
+
+            // PreparedStatements can use variables and are more efficient
+            preparedStatement = connect.prepareStatement("insert into  feedback.COMMENTS values (default, ?, ?, ?, ? , ?, ?)");
+            // "myuser, webpage, datum, summary, COMMENTS from feedback.COMMENTS");
+            // Parameters start with 1
+            preparedStatement.setString(1, "Test");
+            preparedStatement.setString(2, "TestEmail");
+            preparedStatement.setString(3, "TestWebpage");
+            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
+            preparedStatement.setString(5, "TestSummary");
+            preparedStatement.setString(6, "TestComment");
+            preparedStatement.executeUpdate();
+
+            preparedStatement = connect
+                    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from feedback.COMMENTS");
+            resultSet = preparedStatement.executeQuery();
+            writeResultSet(resultSet);
+
+            // Remove again the insert comment
+            preparedStatement = connect
+                    .prepareStatement("delete from feedback.COMMENTS where myuser= ? ; ");
+            preparedStatement.setString(1, "Test");
+            preparedStatement.executeUpdate();
+
+            resultSet = statement
+                    .executeQuery("select * from feedback.COMMENTS");
+            writeMetaData(resultSet);
+
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            close();
+        }
+
+    }
+
+    private void writeMetaData(ResultSet resultSet) throws SQLException {
+        //   Now get some metadata from the database
+        // Result set get the result of the SQL query
+
+        System.out.println("The columns in the table are: ");
+
+        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
+        for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+            System.out.println("Column " + i + " " + resultSet.getMetaData().getColumnName(i));
+        }
+    }
+
+    private void writeResultSet(ResultSet resultSet) throws SQLException {
+        // ResultSet is initially before the first data set
+        while (resultSet.next()) {
+            // It is possible to get the columns via name
+            // also possible to get the columns via the column number
+            // which starts at 1
+            // e.g. resultSet.getSTring(2);
+            String user = resultSet.getString("myuser");
+            String website = resultSet.getString("webpage");
+            String summary = resultSet.getString("summary");
+            Date date = resultSet.getDate("datum");
+            String comment = resultSet.getString("comments");
+            System.out.println("User: " + user);
+            System.out.println("Website: " + website);
+            System.out.println("Summary: " + summary);
+            System.out.println("Date: " + date);
+            System.out.println("Comment: " + comment);
+        }
+    }
+
+    // You need to close the resultSet
+    private void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+        }
+    }
+
+    
+    
+    
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletDB</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletDB at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            readDataBase();
+        } catch (Exception ex) {
+            Logger.getLogger(ServletDB.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ MySQLandJDBCWeb/web/index.jsp
@@ -0,0 +1,20 @@
+<%-- 
+    Document   : index
+    Created on : 11-nov-2013, 16.30.08
+    Author     : ANDre1
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+        <form action="ServletDB" method="POST">
+            <input type="submit" value="Prova a leggere dal database MySQL e vedi cosa succede">
+        </form>
+    </body>
+</html>
